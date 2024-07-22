6adc841bb1b39a0ede3066a0dd93999bd0b6fd4b
Hamid <ghhamid80@gmail.com>
2024-07-22 10:03:10 +0330

    adding folders and changes

---
+++ src/pages/Archive.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+function Archive() {
+    return <h1>hello audio to text</h1>;
+}
+
+export default Archive;
---
+++ src/pages/AudioToText.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+
+function AudioToText() {
+    return <h1>hello audio to text</h1>;
+}
+
+export default AudioToText;
